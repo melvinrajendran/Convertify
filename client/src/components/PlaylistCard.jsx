5c950b13fee51c9f1374ed4ceff826e12d2ef0bc
melvinrajendran <melvin.rajendran@gmail.com>
2022-01-07 16:09:32 -0500

    Finish Profile UI

--- client/src/components/PlaylistCard.jsx
+++ client/src/components/PlaylistCard.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Card } from "react-bootstrap";
+import "./PlaylistCard.css";
 
 const PlaylistCard = (props) => {
-  return <img className="m-5 img-fluid" src={props.info.images[0].url} width="225" height="225" alt="playlist" />;
+  return (
+    <Card className="playlist-card">
+      <Card.Img className="rounded pt-4 pb-1 px-4" variant="top" src={props.info.images[0].url} alt="playlist" />
+      <Card.Body>
+        <Card.Title className="ps-2">
+          <h5>{props.info.name}</h5>
+        </Card.Title>
+      </Card.Body>
+    </Card>
+  );
 };
 
 export default PlaylistCard;
--- client/src/components/Playlists.jsx
+++ client/src/components/Playlists.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import { Row, Col } from "react-bootstrap";
 import PlaylistCard from "./PlaylistCard";
 
 const Playlists = (props) => {
   return (
     <div>
       <h2 className="ps-5 display-5 bold-title">Playlists</h2>
-      <div className="text-center">{props.playlists && props.playlists.items.map((playlist, index) => <PlaylistCard key={index} info={playlist} />)}</div>
+      <Row className="g-4 p-4">
+        {props.playlists &&
+          props.playlists.items.map((playlist, index) => {
+            return (
+              <Col className="d-flex justify-content-center mb-md-3" key={index}>
+                <PlaylistCard key={index} info={playlist} />
+              </Col>
+            );
+          })}
+      </Row>
     </div>
   );
 };
