cb325067e8f0faab55c60035444469a14da75937
melvinrajendran <melvin.rajendran@gmail.com>
2022-01-09 16:50:54 -0500

    Update Login component again

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -10,7 +10,7 @@ const Login = () => {
   return (
     <FullScreenContainer>
       <h1 className="bold-title display-1">Convertify</h1>
-      <p className="fs-5 mb-5 px-5 description-text gray-text">The easiest way to convert your Spotify playlists to clean or explicit.</p>
+      <p className="fs-5 mb-5 px-5 description-text gray-text">The easiest way to convert your Spotify playlists from explicit to clean.</p>
       <PillButton text="Log in to Spotify" href={LOGIN_URI} />
     </FullScreenContainer>
   );
