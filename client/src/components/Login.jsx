a1c63570d47875fe337d75b6b7d7bce36029c30b
melvinrajendran <melvin.rajendran@gmail.com>
2022-12-31 19:18:33 -0500

    Remove Footer

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
     <FullScreenContainer>
       <h1 className="bold-title display-1">Convertify</h1>
       <p className="fs-5 mb-5 px-5 description-text gray-text">The easiest way to convert your Spotify playlists between explicit and clean.</p>
-      <PillButton text="Log in to Spotify" href={LOGIN_URI} />
+      <PillButton text="Log in to Spotify" href={LOGIN_URI} title="Log in to Spotify" />
     </FullScreenContainer>
   );
 };
