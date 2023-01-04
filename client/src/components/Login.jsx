b1e565320b241c05817cd0f5af2683b8cf311636
melvinrajendran <melvin.rajendran@gmail.com>
2023-01-04 15:22:56 -0500

    Update config variables in Login component

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -3,15 +3,13 @@ import FullScreenContainer from "./FullScreenContainer";
 import PillButton from "./PillButton";
 
 const Login = () => {
-  const LOGIN_URI = process.env.LOGIN_URI;
-
   useEffect(() => (document.title = "Convertify | Login"), []);
 
   return (
     <FullScreenContainer>
       <h1 className="bold-title display-1">Convertify</h1>
       <p className="fs-5 mb-5 px-5 description-text gray-text">The easiest way to convert your Spotify playlists between explicit and clean.</p>
-      <PillButton text="Log in to Spotify" href={LOGIN_URI} title="Log in to Spotify" />
+      <PillButton text="Log in to Spotify" href={process.env.LOGIN_URI} title="Log in to Spotify" />
     </FullScreenContainer>
   );
 };
