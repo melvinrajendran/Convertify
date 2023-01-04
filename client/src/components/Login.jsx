ccb07ee8c40f4c44c0ff49e180de03430a151f30
melvinrajendran <melvin.rajendran@gmail.com>
2023-01-04 16:42:51 -0500

    Remove Heroku integration

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -3,7 +3,7 @@ import FullScreenContainer from "./FullScreenContainer";
 import PillButton from "./PillButton";
 
 const Login = () => {
-  const REACT_APP_LOGIN_URI = process.env.REACT_APP_LOGIN_URI;
+  const LOGIN_URI = process.env.LOGIN_URI;
 
   useEffect(() => (document.title = "Convertify | Login"), []);
 
@@ -11,7 +11,7 @@ const Login = () => {
     <FullScreenContainer>
       <h1 className="bold-title display-1">Convertify</h1>
       <p className="fs-5 mb-5 px-5 description-text gray-text">The easiest way to convert your Spotify playlists between explicit and clean.</p>
-      <PillButton text="Log in to Spotify" href={REACT_APP_LOGIN_URI} title="Log in to Spotify" />
+      <PillButton text="Log in to Spotify" href={LOGIN_URI} title="Log in to Spotify" />
     </FullScreenContainer>
   );
 };
